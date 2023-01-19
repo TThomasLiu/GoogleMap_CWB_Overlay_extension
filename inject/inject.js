cb9d17a5c1dab14398f8b5c2735e463eeccd92fd
TTTTTTT THOMAS <0716thomas@gmail.com>
2023-01-20 00:10:07 +0800

    add comments

--- inject/inject.js
+++ inject/inject.js
@@ -28,6 +28,12 @@ let radarOverlays = {};
 let settingElement;
 let mouseStartCoord = undefined;
 
+//--------------------------------
+//
+// Initialization functions
+//
+//--------------------------------
+
 // loading function
 function LoadInject(){
 	console.log("inject loaded");
@@ -59,191 +65,6 @@ function LoadInject(){
 	overlayLayer.style.setProperty("--activeOpacity", "0.5");
 
 	CreateSetting();
-}
-
-// change listener
-function ChangeListener(){
-	if(oldUrl !== document.URL){
-		let strProc;
-		let width = window.innerWidth / 2;
-		let height = window.innerHeight / 2;
-		let horiDegDiv;
-		let vertDegDiv;
-		let mpx;
-
-		oldUrl = document.URL;
-		
-
-		//extract position information from query
-		strProc = oldUrl.split("@")[1].split("?")[0].split(',');
-		strProc[0] = parseFloat(strProc[0]);
-		strProc[1] = parseFloat(strProc[1]);
-		strProc[2] = parseFloat(strProc[2].replace('z', ""));
-
-		mpx = 156543.03392 * Math.cos(strProc[0] * Math.PI / 180) / Math.pow(2, strProc[2]);
-		horiDegDiv = 2 * Math.asin(width * mpx / (2 * earthRad)) * 180 / Math.PI / 0.9173128938;
-		vertDegDiv = 2 * Math.asin(height * mpx / (2 * earthRad)) * 180 / Math.PI;
-
-
-		mapParam.lat = strProc[0];
-		mapParam.lon = strProc[1];
-		mapParam.minLat = strProc[0] - vertDegDiv;
-		mapParam.maxLat = strProc[0] + vertDegDiv;
-		mapParam.minLon = strProc[1] - horiDegDiv;
-		mapParam.maxLon = strProc[1] + horiDegDiv;
-		mapParam.width = window.innerWidth;
-		mapParam.height = window.innerHeight;
-		mapParam.lat2px = height / (vertDegDiv);
-		mapParam.lon2px = width / (horiDegDiv);
-
-		UpdateOverlay();
-	}
-}
-
-// curser change callback
-function CursorChangeCallback(mutationList, o){
-	for (const mutation of mutationList) {
-		if (mutation.type === 'attributes' && mutation.attributeName === "style") {
-			if(cursorTargetElement.style.cursor === "auto"){
-				//showOverlayTimer = setTimeout(ShowOverlay, 5000);
-				document.getElementById("scene").removeEventListener("mousemove", OncursorMove);
-				if(mouseStartCoord !== undefined){
-					//MoveOverlayMomentum(mouseStartCoord.deltaMouseCoord.x, mouseStartCoord.deltaMouseCoord.y, mouseStartCoord.deltaMouseCoord.t);
-					HideOverlay();
-				}
-				mouseStartCoord = undefined;
-
-
-			}else if(cursorTargetElement.style.cursor === "move"){
-				if(mouseStartCoord === undefined){
-					mouseStartCoord = null;
-					document.getElementById("scene").addEventListener("mousemove", OncursorMove);
-				}
-			}
-		}
-	}
-};
-
-function OncursorMove(event){
-	let overlays = overlayLayer.getElementsByClassName("overlay");
-	if(!mouseStartCoord){
-		mouseStartCoord = {};
-		mouseStartCoord.x = event.clientX;
-		mouseStartCoord.y = event.clientY;
-		mouseStartCoord.oldCoord = [];
-		mouseStartCoord.lastMouseCoord = {
-			x:event.clientX,
-			y:event.clientY,
-			t:event.timeStamp
-		};
-		mouseStartCoord.deltaMouseCoord = {
-			x:0,
-			y:0,
-			t:0
-		};
-		for(let i = 0;i<overlays.length;++i){
-			let rect = overlays[i].getBoundingClientRect();
-			mouseStartCoord.oldCoord.push({
-					x:rect.x,
-					y:rect.y
-				});
-		}
-		return;
-	}
-
-	for(let i = 0;i<overlays.length;++i){
-		overlays[i].style.top = `${mouseStartCoord.oldCoord[i].y + event.clientY - mouseStartCoord.y}px`;
-		overlays[i].style.left = `${mouseStartCoord.oldCoord[i].x + event.clientX - mouseStartCoord.x}px`;
-	}
-
-	mouseStartCoord.deltaMouseCoord = {
-		x : mouseStartCoord.deltaMouseCoord.x * 0.1 + (event.clientX - mouseStartCoord.lastMouseCoord.x) * 0.9,
-		y : mouseStartCoord.deltaMouseCoord.y * 0.1 + (event.clientY - mouseStartCoord.lastMouseCoord.y)* 0.9,
-		t : mouseStartCoord.deltaMouseCoord.t * 0.1 + (event.timeStamp - mouseStartCoord.lastMouseCoord.t)* 0.9
-	}
-
-	mouseStartCoord.lastMouseCoord = {
-		x:event.clientX,
-		y:event.clientY,
-		t:event.timeStamp
-	};
-}
-
-function HideOverlayOnZoom(){
-	HideOverlay();
-	if(showOverlayTimer){
-		clearTimeout(showOverlayTimer);
-	}
-	showOverlayTimer = setTimeout(ShowOverlay, 5000);
-}
-
-function HideOverlay(){
-	//hide overlay
-	isOverlayClose = true;
-	overlayLayer.classList.remove("active");
-}
-
-function ShowOverlay(){
-	//show overlay
-	if(forceClose)return;
-	isOverlayClose = false;
-	overlayLayer.classList.add("active");
-}
-
-function UpdateOverlay(){
-	if(showOverlayTimer){
-		clearTimeout(showOverlayTimer);
-		showOverlayTimer = undefined;
-	}
-
-	//show overlay
-	if(isOverlayClose){
-		ShowOverlay();
-	}
-
-	let overlays = overlayLayer.getElementsByClassName("overlay");
-
-	for(let i = 0;i<overlays.length;++i){
-		UpdateSingleOverlay(overlays[i]);
-	}
-	
-	console.log("update overlay");
-}
-
-function CreateOverlay(maxLat, minLon, latDiff, lonDiff, content, id, active=true){
-	let newOverlay = document.createElement('div');
-	newOverlay.classList.add("overlay");
-	overlayLayer.appendChild(newOverlay);
-	newOverlay.setAttribute("lat", maxLat  + latDiff / 2);
-	newOverlay.setAttribute("lon", minLon - lonDiff / 2);
-	newOverlay.setAttribute("latDiff", latDiff);
-	newOverlay.setAttribute("lonDiff", lonDiff);
-
-	newOverlay.innerHTML = content;
-
-	newOverlay.id = id;
-
-	if(active){
-		newOverlay.classList.add("active");
-	}
-
-	UpdateSingleOverlay(newOverlay);
-
-	return newOverlay;
-}
-
-function UpdateSingleOverlay(overlay){
-	let lat = overlay.getAttribute("lat");
-	let lon = overlay.getAttribute("lon");
-	let latDiff = overlay.getAttribute("latDiff");
-	let lonDiff = overlay.getAttribute("lonDiff");
-	let width = lonDiff * mapParam.lon2px;
-	let height = latDiff * mapParam.lat2px;
-
-	overlay.style.width = `${width}px`;
-	overlay.style.height = `${height}px`;
-	overlay.style.top = `${-(lat - mapParam.lat) / latDiff * height + mapParam.height / 2}px`;
-	overlay.style.left = `${(lon - mapParam.lon) / lonDiff * width  + mapParam.width / 2 }px`;
 }
 
 function CreateSetting(){
@@ -515,6 +336,202 @@ function CreateSetting(){
 	}
 }
 
+function CreateOverlay(maxLat, minLon, latDiff, lonDiff, content, id, active=true){
+	let newOverlay = document.createElement('div');
+	newOverlay.classList.add("overlay");
+	overlayLayer.appendChild(newOverlay);
+	newOverlay.setAttribute("lat", maxLat  + latDiff / 2);
+	newOverlay.setAttribute("lon", minLon - lonDiff / 2);
+	newOverlay.setAttribute("latDiff", latDiff);
+	newOverlay.setAttribute("lonDiff", lonDiff);
+
+	newOverlay.innerHTML = content;
+
+	newOverlay.id = id;
+
+	if(active){
+		newOverlay.classList.add("active");
+	}
+
+	UpdateSingleOverlay(newOverlay);
+
+	return newOverlay;
+}
+
+
+//--------------------------------
+//
+// Map utility functions
+//
+//--------------------------------
+// change listener
+function ChangeListener(){
+	if(oldUrl !== document.URL){
+		let strProc;
+		let width = window.innerWidth / 2;
+		let height = window.innerHeight / 2;
+		let horiDegDiv;
+		let vertDegDiv;
+		let mpx;
+
+		oldUrl = document.URL;
+		
+
+		//extract position information from query
+		strProc = oldUrl.split("@")[1].split("?")[0].split(',');
+		strProc[0] = parseFloat(strProc[0]);
+		strProc[1] = parseFloat(strProc[1]);
+		strProc[2] = parseFloat(strProc[2].replace('z', ""));
+
+		mpx = 156543.03392 * Math.cos(strProc[0] * Math.PI / 180) / Math.pow(2, strProc[2]);
+		horiDegDiv = 2 * Math.asin(width * mpx / (2 * earthRad)) * 180 / Math.PI / 0.9173128938;
+		vertDegDiv = 2 * Math.asin(height * mpx / (2 * earthRad)) * 180 / Math.PI;
+
+
+		mapParam.lat = strProc[0];
+		mapParam.lon = strProc[1];
+		mapParam.minLat = strProc[0] - vertDegDiv;
+		mapParam.maxLat = strProc[0] + vertDegDiv;
+		mapParam.minLon = strProc[1] - horiDegDiv;
+		mapParam.maxLon = strProc[1] + horiDegDiv;
+		mapParam.width = window.innerWidth;
+		mapParam.height = window.innerHeight;
+		mapParam.lat2px = height / (vertDegDiv);
+		mapParam.lon2px = width / (horiDegDiv);
+
+		UpdateOverlay();
+	}
+}
+
+// curser change callback
+function CursorChangeCallback(mutationList, o){
+	for (const mutation of mutationList) {
+		if (mutation.type === 'attributes' && mutation.attributeName === "style") {
+			if(cursorTargetElement.style.cursor === "auto"){
+				//showOverlayTimer = setTimeout(ShowOverlay, 5000);
+				document.getElementById("scene").removeEventListener("mousemove", OncursorMove);
+				if(mouseStartCoord !== undefined){
+					//MoveOverlayMomentum(mouseStartCoord.deltaMouseCoord.x, mouseStartCoord.deltaMouseCoord.y, mouseStartCoord.deltaMouseCoord.t);
+					HideOverlay();
+				}
+				mouseStartCoord = undefined;
+
+
+			}else if(cursorTargetElement.style.cursor === "move"){
+				if(mouseStartCoord === undefined){
+					mouseStartCoord = null;
+					document.getElementById("scene").addEventListener("mousemove", OncursorMove);
+				}
+			}
+		}
+	}
+};
+
+function OncursorMove(event){
+	let overlays = overlayLayer.getElementsByClassName("overlay");
+	if(!mouseStartCoord){
+		mouseStartCoord = {};
+		mouseStartCoord.x = event.clientX;
+		mouseStartCoord.y = event.clientY;
+		mouseStartCoord.oldCoord = [];
+		mouseStartCoord.lastMouseCoord = {
+			x:event.clientX,
+			y:event.clientY,
+			t:event.timeStamp
+		};
+		mouseStartCoord.deltaMouseCoord = {
+			x:0,
+			y:0,
+			t:0
+		};
+		for(let i = 0;i<overlays.length;++i){
+			let rect = overlays[i].getBoundingClientRect();
+			mouseStartCoord.oldCoord.push({
+					x:rect.x,
+					y:rect.y
+				});
+		}
+		return;
+	}
+
+	for(let i = 0;i<overlays.length;++i){
+		overlays[i].style.top = `${mouseStartCoord.oldCoord[i].y + event.clientY - mouseStartCoord.y}px`;
+		overlays[i].style.left = `${mouseStartCoord.oldCoord[i].x + event.clientX - mouseStartCoord.x}px`;
+	}
+
+	mouseStartCoord.deltaMouseCoord = {
+		x : mouseStartCoord.deltaMouseCoord.x * 0.1 + (event.clientX - mouseStartCoord.lastMouseCoord.x) * 0.9,
+		y : mouseStartCoord.deltaMouseCoord.y * 0.1 + (event.clientY - mouseStartCoord.lastMouseCoord.y)* 0.9,
+		t : mouseStartCoord.deltaMouseCoord.t * 0.1 + (event.timeStamp - mouseStartCoord.lastMouseCoord.t)* 0.9
+	}
+
+	mouseStartCoord.lastMouseCoord = {
+		x:event.clientX,
+		y:event.clientY,
+		t:event.timeStamp
+	};
+}
+
+function HideOverlayOnZoom(){
+	HideOverlay();
+	if(showOverlayTimer){
+		clearTimeout(showOverlayTimer);
+	}
+	showOverlayTimer = setTimeout(ShowOverlay, 5000);
+}
+
+//----------------------------------
+//
+// Overlay functions
+//
+//----------------------------------
+function HideOverlay(){
+	//hide overlay
+	isOverlayClose = true;
+	overlayLayer.classList.remove("active");
+}
+
+function ShowOverlay(){
+	//show overlay
+	if(forceClose)return;
+	isOverlayClose = false;
+	overlayLayer.classList.add("active");
+}
+
+function UpdateOverlay(){
+	if(showOverlayTimer){
+		clearTimeout(showOverlayTimer);
+		showOverlayTimer = undefined;
+	}
+
+	//show overlay
+	if(isOverlayClose){
+		ShowOverlay();
+	}
+
+	let overlays = overlayLayer.getElementsByClassName("overlay");
+
+	for(let i = 0;i<overlays.length;++i){
+		UpdateSingleOverlay(overlays[i]);
+	}
+	
+	console.log("update overlay");
+}
+
+function UpdateSingleOverlay(overlay){
+	let lat = overlay.getAttribute("lat");
+	let lon = overlay.getAttribute("lon");
+	let latDiff = overlay.getAttribute("latDiff");
+	let lonDiff = overlay.getAttribute("lonDiff");
+	let width = lonDiff * mapParam.lon2px;
+	let height = latDiff * mapParam.lat2px;
+
+	overlay.style.width = `${width}px`;
+	overlay.style.height = `${height}px`;
+	overlay.style.top = `${-(lat - mapParam.lat) / latDiff * height + mapParam.height / 2}px`;
+	overlay.style.left = `${(lon - mapParam.lon) / lonDiff * width  + mapParam.width / 2 }px`;
+}
+
 function ToggleOverlay(){
 	if(overlayLayer.classList.contains("active")){
 		forceClose = true;
@@ -534,6 +551,12 @@ function ToggleSingleOverlay(event){
 	}
 }
 
+
+//-----------------------------------------
+//
+// Button Actions
+//
+//-----------------------------------------
 function ToggleTaiwanRadarBackground(event){
 	let target = document.getElementById("taiwan");
 	if(event.target.checked){
